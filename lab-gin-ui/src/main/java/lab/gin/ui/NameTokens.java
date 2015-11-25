136be04d381751082fcf0c90a5b1edb86965f0a0
Walison Moreira <walison.moreira@gmail.com>
2015-11-25 18:41:39 -0200

    Exclusão completa do GWTP e preparação do GIN.

--- lab-gin-ui/src/main/java/lab/gin/ui/NameTokens.java
+++
@@ -1,5 +0,0 @@
-package lab.gin.ui;
-
-public class NameTokens {
-  public static final String HOME = "/";
-}
---
+++ lab-gin-ui/src/main/java/lab/gin/ui/UiEntryPoint.java
@@ -0,0 +1,12 @@
+package lab.gin.ui;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class UiEntryPoint implements EntryPoint {
+
+  @Override
+  public void onModuleLoad() {
+    Window.alert("Lab GIN.");
+  }
+}
